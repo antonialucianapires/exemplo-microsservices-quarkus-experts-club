d34e12e56842b197b53809139b3f3142ff6b3887
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-08-29 21:54:51 -0300

    feat: padrão Application Metricas

--- api/src/main/java/com/antonialucianapires/GreetingResource.java
+++ api/src/main/java/com/antonialucianapires/GreetingResource.java
@@ -1,5 +1,7 @@
 package com.antonialucianapires;
 
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.inject.Inject;
@@ -16,9 +18,16 @@ public class GreetingResource {
     @RestClient
     TimeService timeService;
 
+    @Counted( // métricas de requisição
+	name = "time.count")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        return getTime();
+    }
+
+    @Timed(name = "time.targe") //estatística de tempo
+    public String getTime() {
         return timeService.getTime();
     }
 }
